53c9df14cdacf65dcc47dbefb8ee4d5df824598f
Cornelia Ärlerud <cornelia.arlerud@gmail.com>
2026-04-09 21:36:59 +0200

    feat(python): add ip and port validation

--- networktool.py
+++ networktool.py
@@ -1,54 +1,66 @@
-def show_menu():
+def show_menu():                    # visar menyn för användare
     print('=== NÄTVERKSVERKTYG ===')
     print('1. Validera IP-adress')
     print('2. Validera port')
     print('3. Visa logg')
     print('4. Avsluta.')
 
+def validate_ip(ip):
+     ip = ip.strip()                # tar bort mellanslag i början/slutet
+     octets = ip.split(".")         # delar upp strängen vid varje punkt
+     if len(octets) != 4: 
+          return False              # om det inte är exakt 4 delar: avslutar funktionen och ger "ogiltig IP" 
+     for octet in octets:           # loopar igenom varje del
+          if not octet.isdigit(): 
+               return False         # om det inte är siffror: avslutar funktionen och ger "ogiltig IP"
+     if not 0 <= int(octet) <= 255:
+              return False #       # om det är utanför intervallet: avslutar funktionen och ger "ogiltig IP"
+     return True                   # om allt ovan klaras: Giltig IP! 
 
-    while True: 
-         show_menu()
-         choice = input('Gör ett val (1-4): ')
+def validate_port(port):
+     port = port.strip()
+
+     if not port.isdigit():
+          return False
+     
+     port = int(port)
+
+     if 1 <= port <= 65535:
+          return True
+     else:
+          return False
+     
+while True: 
+     show_menu()
+     choice = input('Gör ett val (1-4): ')
          
-         # Choice 1 - Validera IP-adress: följer formatet x.x.x.x där varje oktett är ett heltal mellan 0 och 255. 
-    if choice == '1': 
-        ip = input('Ange IP-adress: ')
-        octets = ip.split(".")
-        if len(octets) == 4 and all(0 <= int(octet) <= 255 for octet in octets):
-            print(ip + ' är en giltig IP-adress.')
-        else: 
-            print(ip + ' är en ogiltig IP-adress.')
-
-    # Choice 2 - Validerar port (giltig/ogiltig)
-
-    if choice == '2':
-         port = input('Ange port: ')
-         if port.isdigit ():
-              port = int(port)
-              if 1 <= port <= 65535:
-                   print(str(port) + ' är en giltig port!')  
-                   print(str(port) + ' är en ogiltig port.')  
-              else:
-                   print('Input är ogiltig! Försök igen med ett nummer! :)')
-
-    # Choice 3 - Visa loggen: 
-    import logging
-
-    logging.basicConfig(
-         level=logging.INFO,
-         format='%(asctime)s - %(levelname)s - %(message)s',
-         datefmt='%Y-%m-%d %H:%M:%S'
-    )
-
-    logg = []
-
-    if choice == '3':
-         print('Logg:')
-         for rad in logg:
-              print(rad)
-                   
-    # Choice 4 - Avslutar programmet
-    elif choice == '4':
-            print('Avslutar...')
-            break
+     if choice == '1': 
+          ip = input('Ange IP-adress: ')
+          if validate_ip(ip):
+               print(f'{ip} är en giltig IP-adress. \n')
+          else:
+               print(f'{ip} är en ogiltig IP-adress. \n')
+               
+     elif choice == '2':
+          port = input('Ange port: ')
+          if validate_port(port):
+               print(f'Port {port}' ' är giltig!')  
+          else:
+               print(f'Port {port} är ogiltig.')  
+                              
+          import logging
+          logging.basicConfig(
+          level=logging.INFO,
+          format='%(asctime)s - %(levelname)s - %(message)s',
+          datefmt='%Y-%m-%d %H:%M:%S')                         
+          logg = []
+                              
+     if choice == '3':
+          print('Logg:')
+          for rad in logg:
+               print(rad)
+     elif choice == '4': 
+          print('Avslutar...')
+          break 
+                              
     
