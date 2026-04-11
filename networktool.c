f1753a5ae8d7ffd8b72069ad69933ebc3643e10c
Cornelia Ärlerud <cornelia.arlerud@gmail.com>
2026-04-12 01:21:41 +0200

    feat(c): add menu option using scanf

--- networktool.c
+++ networktool.c
@@ -6,6 +6,18 @@
 
 int main() {
 
+	int choice;
+
+	printf("=== NÄTVERKSVERKTYG ===\n");
+	printf("1. Validera IP-adress\n");
+	printf("2. Validera port\n");
+	printf("3. Visa logg\n");
+	printf("Avsluta\n");
+
+	printf("Gör ett val (1-4): ");
+	scanf("%d", &choice);
+
+	printf("validerar: %d \n", choice);
 
 
 
